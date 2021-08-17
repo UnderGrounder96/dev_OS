59311e143f39b3b37ba4b2c6d4061d4ee1245577
Lucio Afonso <lucio.afonso@ibm.com>
2021-08-17 20:49:00 +0200

    Updated deps.sh

--- lib/deps.sh
+++ lib/deps.sh
@@ -6,9 +6,34 @@
 
 set -euo pipefail
 
-# groupinstall
-sudo dnf group install -y "C Development Tools and Libraries"
-sudo dnf group install -y "Development Tools"
+function group_installs(){
+    _logger_info "Performing groupinstalls"
+    sudo dnf group install -y "C Development Tools and Libraries"
+    sudo dnf group install -y "Development Tools"
+}
 
-# soloinstall
-# sudo dnf install -y texinfo 
+function solo_installs(){
+    _logger_info "Performing soloinstalls"
+    # sudo dnf install -y texinfo
+}
+
+function check_yaac(){
+    _logger_info "Checking yaac x bison relation"
+    rpm -qf `which yacc`
+
+    sudo dnf remove -y byacc
+    sudo dnf reinstall -y bison
+
+    sudo ln -s `which bison` /bin/yacc
+}
+
+function main(){
+    group_installs
+    solo_installs
+
+    check_yaac
+
+    exit 0
+}
+
+main
