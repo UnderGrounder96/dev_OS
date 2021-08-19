d5f350cb0a036960eac87e8a83f646f41e9424d4
Lucio Afonso <lucio.afonso@ibm.com>
2021-08-19 18:40:18 +0200

    Added create_user

--- lib/deps.sh
+++ lib/deps.sh
@@ -27,11 +27,30 @@ function check_yaac(){
     sudo ln -s `which bison` /bin/yacc
 }
 
+function create_user(){
+    _logger_info "Handling build user creation"
+    export BUILD_USER="byol"
+
+    sudo useradd --create-home --skel /dev/null $BUILD_USER
+    sudo usermod $BUILD_USER -aG wheel
+
+    echo 'exec env -i HOME=$HOME TERM=$TERM PS1="[\u@\h \W]\$\n" /bin/bash' | sudo -u $BUILD_USER tee /home/$BUILD_USER/.bash_profile
+
+    sudo -u $BUILD_USER tee /home/$BUILD_USER/.bashrc <<EOF 1>/dev/null
+        set +h # disables history command path
+        umask 022 # sets file creation permission
+        export LC_ALL=POSIX
+        export PATH=/tools/bin:/bin:/usr/bin
+EOF
+}
+
 function main(){
     group_installs
     solo_installs
 
     check_yaac
+
+    create_user
 
     exit 0
 }
