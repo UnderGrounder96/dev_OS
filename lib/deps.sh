a6b88f7404fb7b0bf3277160e87de4a03d82dfe7
Lucio Afonso <lucio.afonso@ibm.com>
2021-08-22 01:10:39 +0200

    Added build_disk_mount

--- lib/deps.sh
+++ lib/deps.sh
@@ -40,9 +40,48 @@ function create_user(){
         set +h # disables history command path
         umask 022 # sets file creation permission
         export LC_ALL=POSIX
+        export BROOT=/build
         export PATH=/tools/bin:/bin:/usr/bin
 EOF
 }
+
+function build_disk_mount(){
+    _logger_info "Handling build_disk"
+    export BROOT="/build"
+
+    sudo mkdir -vp $BROOT/boot
+
+    # Labeling build_disk
+    sudo parted --script /dev/sdb mklabel gpt
+
+    # EFI/GRUB partition
+    sudo parted --script /dev/sdb mkpart primary 0% 120MB
+    sudo parted --script /dev/sdb set 1 bios_grub on # enables bios_grub flag in part1
+    sudo mkfs.ext4 -L DESTGRUB /dev/sdb1
+
+    # '/boot' partition
+    sudo parted --script /dev/sdb mkpart primary 120MB 666MB
+    sudo mkfs.xfs -L DESTBOOT /dev/sdb2
+    sudo mount -t xfs --label DESTBOOT $BROOT/boot
+
+    # '/' root file system partition
+    sudo parted --script /dev/sdb mkpart primary 666MB 28GB
+    sudo mkfs.xfs -L DESTROOT /dev/sdb3
+    sudo mount -t xfs --label DESTROOT $BROOT
+
+    # swap partition
+    sudo parted --script /dev/sdb mkpart primary 28GB  100%
+    sudo mkswap --label DESTSWAP /dev/sdb4
+    sudo swapon LABEL=DESTSWAP
+
+
+    _logger_info "Sanity check"
+    mount | grep 'sdb'
+    lsblk
+
+    sleep 15s
+}
+
 
 function main(){
     group_installs
@@ -51,6 +90,8 @@ function main(){
     check_yaac
 
     create_user
+
+    build_disk_mount
 
     exit 0
 }
