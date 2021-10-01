2677a05d72755dd63fd337d51d040ab7869edb9d
Lucio Afonso <lucioafonso@icloud.com>
2021-10-01 19:37:39 +0000

    Updated build/bin.sh

--- build/bin.sh
+++ build/bin.sh
@@ -9,49 +9,32 @@ set -euo pipefail
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 LOG_FILE="$ROOT_DIR/logs/bin-$(date '+%F_%T').log"
 
-export ROOT_DIR
-
 source $ROOT_DIR/configs/common.sh
 
-function prepare_tmp_dir(){
-    _logger_info "Preparing tmp dir"
-    export TMP_DIR=$(mktemp -d)
-    pushd "$TMP_DIR"
-}
-
-function clean_tmp_dir(){
-    _logger_info "Cleaning tmp dir"
-    popd
-    # rm -rf "$TMP_DIR"
-}
-
-
 function deps_install(){
-    bash $ROOT_DIR/build/deps.sh
+    env ROOT_DIR="$ROOT_DIR" bash $ROOT_DIR/build/deps.sh
 }
 
 function setup_build(){
     cd $BROOT/source/build
-    sudo -u $BUSER env ROOT_DIR="$ROOT_DIR" bash $ROOT_DIR/build/setup.sh $ROOT_DIR/configs/common.sh
+
+    sudo -u $BUSER env ROOT_DIR="$ROOT_DIR"  \
+      bash $ROOT_DIR/build/setup.sh $ROOT_DIR/configs/common.sh
 }
 
 function build_OS(){
-    sudo env ROOT_DIR="$ROOT_DIR" bash $ROOT_DIR/build/build.sh $ROOT_DIR/configs/common.sh
+    env ROOT_DIR="$ROOT_DIR"  \
+      bash $ROOT_DIR/build/build.sh $ROOT_DIR/configs/common.sh
 }
 
 function main(){
-    prepare_tmp_dir
-
     deps_install
 
     setup_build
 
     build_OS
 
-    clean_tmp_dir
-
     exit 0
 }
-
 
 main 2>&1 | tee -a $LOG_FILE
