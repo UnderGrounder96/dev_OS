6cdcf67de7c6734037e899a165a334d5fa47e73c
Lucio Afonso <lucioafonso@icloud.com>
2021-10-01 20:45:46 +0000

    Updated libs/base.sh

--- libs/base.sh
+++ libs/base.sh
@@ -14,12 +14,13 @@ unset BTARGET
 function creating_os_dirs() {
     _logger_info "Creating OS dirs"
 
-    mkdir -vp /{{,s}bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
     mkdir -vp /{media/{floppy,cdrom},srv}
+    mkdir -vp /{{,s}bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
 
     mkdir -vp /usr/libexec
     mkdir -vp /usr/{,local/}{{,s}bin,include,lib,src}
-    mkdir -vp /usr/{,local/}share/{color,dict,doc,info,locale,misc,terminfo,zoneinfo,man/man{1..8}}
+    mkdir -vp /usr/{,local/}share/{color,dict,doc,info,locale,misc}
+    mkdir -vp /usr/{,local/}share/{terminfo,zoneinfo,man/man{1..8}}
 
     mkdir -vp /var/{lib/{color,misc,locate},opt,cache,local,log,mail,spool}
 
@@ -30,31 +31,32 @@ function creating_os_dirs() {
     ln -sfv /run/lock /var/lock
 
     case $(uname -m) in
-        x86_64)
-          ln -sfv lib /lib64
-          ln -sfv lib /usr/lib64
-          ln -sfv lib /usr/local/lib64
-        ;;
+      x86_64)
+        ln -sfv lib /lib64
+        ln -sfv lib /usr/lib64
+        ln -sfv lib /usr/local/lib64
+      ;;
     esac
 }
 
 function create_essential_files(){
-    _logger_info "Creating OS dirs"
-
-    ln -sv /tools/bin/perl /usr/bin
-    ln -sv /tools/bin/{bash,cat,echo,pwd,stty} /bin
-    ln -sv /tools/lib/libgcc_s.so{,.1} /usr/lib
-    ln -sv /tools/lib/libstdc++.so{,.6} /usr/lib
-    ln -sv bash /bin/sh
-
-    ln -sv /proc/self/mounts /etc/mtab
+    _logger_info "Creating essential OS files and symlinks"
 
     touch /var/log/{btmp,lastlog,faillog,wtmp}
 
-    chmod -v 600  /var/log/btmp
     chmod -v 664  /var/log/lastlog
-    chgrp -v utmp /var/log/lastlog
+    chmod -v 600  /var/log/btmp
 
+    ln -sfv /tools/bin/perl /usr/bin
+    ln -sfv /tools/bin/{bash,cat,echo,pwd,stty} /bin
+    ln -sfv /tools/lib/libgcc_s.so{,.1} /usr/lib
+    ln -sfv /tools/lib/libstdc++.so{,.6} /usr/lib
+    ln -sfv bash /bin/sh
+
+    # create symlink for list of the mounted file systems
+    ln -sfv /proc/self/mounts /etc/mtab
+
+    # create default users
     tee /etc/passwd <<EOF
 root:x:0:0:root:/root:/bin/bash
 bin:x:1:1:bin:/dev/null:/bin/false
@@ -63,6 +65,7 @@ messagebus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false
 nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
 EOF
 
+    # create default groups
     tee /etc/group <<EOF
 root:x:0:
 bin:x:1:daemon
@@ -88,6 +91,8 @@ mail:x:34:
 nogroup:x:99:
 users:x:999:
 EOF
+
+    chgrp -v utmp /var/log/lastlog
 }
 
 function main(){
