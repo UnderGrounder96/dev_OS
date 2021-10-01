02588ee4ca3ac07ad13a391d3643268671ddc1c3
Lucio Afonso <lucioafonso@icloud.com>
2021-10-01 20:33:14 +0000

    Updated build/build.sh

--- build/build.sh
+++ build/build.sh
@@ -14,37 +14,40 @@ unset BTARGET
 LOG_FILE="$ROOT_DIR/logs/build-$(date '+%F_%T').log"
 
 function create_build_dirs(){
-    _logger_info "Creating build OS dirs"
+    _logger_info "Creating build OS file system dirs"
 
     mkdir -pv $BROOT/{dev,proc,sys,run}
 }
 
 function create_device_nodes(){
-    _logger_info "Creating device nodes"
+    _logger_info "Creating initial device nodes"
 
     mknod -m 600 $BROOT/dev/console c 5 1
     mknod -m 666 $BROOT/dev/null c 1 3
 }
 
 function mount_build_dirs(){
-    _logger_info "Mounting build dirs"
+    _logger_info "Mounting Virtual Kernel file system"
 
-    mount -v --bind /dev $BROOT/dev
+    mount -v --bind /dev $BROOT/dev # populate /dev
+
     mount -vt devpts devpts $BROOT/dev/pts --options gid=5,mode=620
     mount -vt proc proc $BROOT/proc
     mount -vt sysfs sysfs $BROOT/sys
     mount -vt tmpfs tmpfs $BROOT/run
 
     if [ -h $BROOT/dev/shm ]; then
-        mkdir -vp $BROOT/$(readlink $BROOT/dev/shm)
+      mkdir -vp $BROOT/$(readlink $BROOT/dev/shm)
     fi
 }
 
 function entering_chroot_jail(){
     _logger_info "Entering chroot jail"
 
-    cp -ruv $ROOT_DIR/{libs,configs} $BROOT
+    # copy lib,config files to be used inside chroot jail
+    cp -rfuv $ROOT_DIR/{libs,configs} $BROOT
 
+    # continue build in chroot environment
     chroot "$BROOT" /tools/bin/env -i                 \
       TERM="$TERM"  PS1="\u:\w\$ "  HOME="/root"      \
       PATH="/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin" \
