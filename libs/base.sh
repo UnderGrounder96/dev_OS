588e59ae345dd878d7dc21ea93f012a4a979f217
Lucio Afonso <lucioafonso@icloud.com>
2021-10-05 10:38:56 +0000

    Updated libs/base.sh

--- libs/base.sh
+++ libs/base.sh
@@ -11,8 +11,21 @@ COMMON="${1}"
 source $COMMON
 unset BTARGET
 
+function clean_cwd(){
+    _logger_info "Removing everything from $PWD"
+
+    local cwd=$PWD
+
+    cd $cwd/..
+    rm -rf $cwd
+    mkdir -vp $cwd
+    cd $cwd
+}
+
 function creating_os_dirs() {
     _logger_info "Creating OS dirs"
+
+    cd /source/build
 
     mkdir -vp /{media/{floppy,cdrom},srv}
     mkdir -vp /{{,s}bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
@@ -95,12 +108,137 @@ EOF
     chgrp -v utmp /var/log/lastlog
 }
 
+function install_man_pages(){
+    _logger_info "Installing man-pages"
+
+    pushd ../man-pages-*
+      make --jobs 9 install
+    popd
+}
+
+function install_glibc(){
+    _logger_info "Installing GlibC"
+
+    # create glibc extra configuration folders
+    mkdir -vp /var/cache/nscd /usr/lib/locale /etc/ld.so.conf.d
+
+    pushd ../glibc-*/
+      sed -e '402a\      *result = local->data.services[database_index];' \
+        -i nss/nss_database.c
+    popd
+
+    ../glibc-*/configure --prefix=/usr  \
+      --disable-werror                  \
+      --enable-kernel=3.2               \
+      --enable-stack-protector=strong   \
+      --with-headers=/tools/include
+
+    make --jobs 9
+
+    make --jobs 9 check || true
+
+    touch /etc/ld.so.conf
+
+    # skips unneeded sanity check
+    sed '/test-installation/s@$(PERL)@echo not running@' -i ../glibc-*/Makefile
+
+    make --jobs 9 install
+
+    cp -fuv ../glibc-*/nscd/nscd.conf /etc/nscd.conf
+
+
+    # install the locales that can make the system respond in a different language
+    localedef -i POSIX -f UTF-8 C.UTF-8 2> /dev/null || true
+    localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
+    localedef -i de_DE -f ISO-8859-1 de_DE
+    localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro
+    localedef -i de_DE -f UTF-8 de_DE.UTF-8
+    localedef -i el_GR -f ISO-8859-7 el_GR
+    localedef -i en_GB -f UTF-8 en_GB.UTF-8
+    localedef -i en_HK -f ISO-8859-1 en_HK
+    localedef -i en_PH -f ISO-8859-1 en_PH
+    localedef -i en_US -f ISO-8859-1 en_US
+    localedef -i en_US -f UTF-8 en_US.UTF-8
+    localedef -i es_MX -f ISO-8859-1 es_MX
+    localedef -i fa_IR -f UTF-8 fa_IR
+    localedef -i fr_FR -f ISO-8859-1 fr_FR
+    localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro
+    localedef -i fr_FR -f UTF-8 fr_FR.UTF-8
+    localedef -i it_IT -f ISO-8859-1 it_IT
+    localedef -i it_IT -f UTF-8 it_IT.UTF-8
+    localedef -i ja_JP -f EUC-JP ja_JP
+    localedef -i ja_JP -f SHIFT_JIS ja_JP.SIJS 2> /dev/null || true
+    localedef -i ja_JP -f UTF-8 ja_JP.UTF-8
+    localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R
+    localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
+    localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
+    localedef -i zh_CN -f GB18030 zh_CN.GB18030
+    localedef -i zh_HK -f BIG5-HKSCS zh_HK.BIG5-HKSCS
+
+    # ---- Configuring GlibC ----
+
+    # Adding nsswitch.conf
+    tee /etc/nsswitch.conf <<EOF
+# Begin /etc/nsswitch.conf
+
+passwd: files
+group: files
+shadow: files
+
+hosts: files dns
+networks: files
+
+protocols: files
+services: files
+ethers: files
+rpc: files
+
+# End /etc/nsswitch.conf
+EOF
+
+    # Configuring the Dynamic Loader
+    tee /etc/ld.so.conf <<EOF
+# Begin /etc/ld.so.conf
+/usr/local/lib
+/opt/lib
+
+# Add an include directory
+include /etc/ld.so.conf.d/*.conf
+EOF
+
+    # Adding time zone data
+    pushd /source
+      ZONEINFO=/usr/share/zoneinfo
+      mkdir -vp $ZONEINFO/{posix,right}
+
+      for tz in etcetera southamerica northamerica europe africa \
+        antarctica asia australasia backward; do
+
+        zic -L /dev/null   -d $ZONEINFO       ${tz}
+        zic -L /dev/null   -d $ZONEINFO/posix ${tz}
+        zic -L leapseconds -d $ZONEINFO/right ${tz}
+      done
+
+      cp -fuv zone.tab zone1970.tab iso3166.tab $ZONEINFO
+      zic -d $ZONEINFO -p America/New_York
+
+      # set time zone info
+      ln -sfv /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+      unset ZONEINFO
+    popd
+}
+
 function main(){
     _logger_info "Executing lib/base.sh"
 
     creating_os_dirs
+    clean_cwd
 
     create_essential_files
+
+    install_man_pages
+
+    install_glibc
 
     exit 0
 }
