48cdf37654bafdce7b57e9bdf089d0d79f5c92e9
Lucio Afonso <lucioafonso@icloud.com>
2021-10-16 12:57:44 +0000

    Updated configs/common.sh

--- configs/common.sh
+++ configs/common.sh
@@ -7,13 +7,27 @@
 # build variables
 BUSER="byol"
 BROOT="/build"
-BTARGET="x86_64-BROOT-linux-gnu"
+BTARGET="$(uname -m)-BROOT-linux-gnu"
 BVERSION="1.0.0"
+CONFIG_SITE=$BROOT/usr/share/config.site
 
 # helper function
 function _logger_info(){
     echo -e "\n[$(date '+%d/%b/%Y %T')]: $*...\n"
 }
 
-export -f _logger_info
-export BUSER BROOT BTARGET BVERSION
+function _unload_build_packages(){
+    _logger_info "Unloading build packages"
+
+    pushd $BROOT/source
+      sudo -u $BUSER cp -fuv $ROOT_DIR/bin/* . # offline packages unloading
+      sudo -u $BUSER find -name "*.tar*" -exec tar -xf {} \;
+
+      pushd glibc-*/
+        patch -Np1 -i ../glibc-*-fhs-1.patch
+      popd
+    popd
+}
+
+export -f _logger_info _unload_build_packages
+export BUSER BROOT BTARGET BVERSION CONFIG_SITE
